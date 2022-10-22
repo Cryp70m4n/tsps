bd6abaf76b422fbc80aee18d9d317bd5a1c04fb0
cryp70m4n <cryp70m4n@gmail.com>
2022-10-22 13:36:55 +0000

    Basic implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,49 @@
+// STDs
 #include <iostream>
+
+// Data structures
 #include <stack>
+#include <unordered_map>
+
 
 int main() {
+    std::unordered_map<int, int> plist;
     std::stack<int> main;
     std::stack<int> execute;
     std::stack<int> waiting;
-
-    for(int i = 0; i <= 10; i++)
-        main.push(i);
     
-    int tmp = main.size();
-    for(int i = 0; i < tmp; i++)
+    int priority = 10;
+    int pid = 4000;
+
+    while(priority >= 0) {
+        plist[pid] = priority;
+        pid+=5;
+        priority--;
+    }
+    
+    for (auto pid : plist)
+        main.push(pid.first);
+    
+    execute.push(main.top());
+    main.pop();
+    
+    int main_size = main.size();
+    for(int i = 0; i < main_size; i++)
     {
-        std::cout << "Element:" << main.top() << "\n";
+        if(plist[main.top()] < plist[execute.top()]) {
+            execute.push(main.top());
+        }
+        else {
+            waiting.push(main.top());
+        }
         main.pop();
     }
+
+    for (auto element : plist)
+        std::cout << "Element::" << element.first << " " << element.second << "\n";
+    
+    std::cout << "\n\n\n";
+    std::cout << "Execution:" << "PID:" << execute.top() << "|PRIORITY:" << plist[execute.top()] << "\n";
 
     return 0;
 }
