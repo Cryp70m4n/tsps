1285777060916ed8c339b5a6dc34736258a0f9e1
cryp70m4n <cryp70m4n@gmail.com>
2022-10-22 15:31:59 +0000

    Added seperated function unit tests needed especially for the waitlist

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,51 @@
 #include <stack>
 #include <unordered_map>
 
-
-int main() {
-    std::unordered_map<int, int> plist;
+int process_scheduler(std::unordered_map<int, int> plist) {
     std::stack<int> main;
     std::stack<int> execute;
     std::stack<int> waiting;
-    
+    while (plist.size() > 0) {
+        for (auto pid : plist)
+            main.push(pid.first);
+        
+        execute.push(main.top());
+        main.pop();
+        
+        int main_size = main.size();
+        for(int i = 0; i < main_size; i++)
+        {
+            if(plist[main.top()] < plist[execute.top()]) {
+                execute.push(main.top());
+            }
+            else {
+                waiting.push(main.top());
+            }
+            main.pop();
+        }
+        
+        while(execute.size()) {
+            //EXECUTE PROCESS HERE
+            std::cout << "Execution:" << "PID:" << execute.top() << "|PRIORITY:" << plist[execute.top()] << "\n";
+            plist.erase(execute.top());
+            execute.pop();
+        }
+
+        int waiting_size = waiting.size();
+        for(int i = 0; i < waiting_size; i++) {
+            if(waiting.top() > 1) {
+                waiting.top() = waiting.top()--;
+            }
+            main.push(waiting.top());
+            waiting.pop();
+        }
+    }
+
+    return 0;
+}
+
+int main() {
+    std::unordered_map<int, int> plist;
     int priority = 10;
     int pid = 4000;
 
@@ -20,30 +58,12 @@ int main() {
         pid+=5;
         priority--;
     }
-    
-    for (auto pid : plist)
-        main.push(pid.first);
-    
-    execute.push(main.top());
-    main.pop();
-    
-    int main_size = main.size();
-    for(int i = 0; i < main_size; i++)
-    {
-        if(plist[main.top()] < plist[execute.top()]) {
-            execute.push(main.top());
-        }
-        else {
-            waiting.push(main.top());
-        }
-        main.pop();
-    }
 
-    for (auto element : plist)
-        std::cout << "Element::" << element.first << " " << element.second << "\n";
+
     
     std::cout << "\n\n\n";
-    std::cout << "Execution:" << "PID:" << execute.top() << "|PRIORITY:" << plist[execute.top()] << "\n";
+
+    std::cout << process_scheduler(plist);
 
     return 0;
 }
